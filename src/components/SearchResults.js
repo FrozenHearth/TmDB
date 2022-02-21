ec7a331b96c40a82ce777700612a0d5a4f4207b3
Vishwanath <FrozenHearth@users.noreply.github.com>
2022-02-22 02:04:32 +0530

    Feat/redux (#8)
    * Installed & setup redux
    
    * Prevented API call when browser back/forward button is pressed

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,31 +1,38 @@
 import { imageURL } from "../config/urls";
 import defaultAvatar from "../assets/icons/defaultAvatar.svg";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { fetchMovie } from "../redux/slices/movieSlice";
 
-export const SearchResults = ({ results }) => {
-  console.log("results", results);
+export const SearchResults = ({ results = [] }) => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  function onMovieSelect(id) {
+    navigate(`/movies/${id}`);
+    dispatch(fetchMovie(id));
+  }
   return (
     <div className="tmdb__search--dropdown font-semibold">
       <ul>
-        {results &&
-          results.map((item, key) => (
-            <li key={key}>
-              <a>{item?.title}</a>
-              <div className="image__container">
-                <img
-                  width="40"
-                  height="40"
-                  src={
-                    item.backdrop_path
-                      ? `${imageURL + item.backdrop_path}`
-                      : item.poster_path
-                      ? `${imageURL + item.poster_path}`
-                      : defaultAvatar
-                  }
-                  alt={item.title}
-                />
-              </div>
-            </li>
-          ))}
+        {results.map((item, key) => (
+          <li onClick={() => onMovieSelect(item.id)} key={key}>
+            <a>{item?.title}</a>
+            <div className="image__container">
+              <img
+                width="40"
+                height="40"
+                src={
+                  item.backdrop_path
+                    ? `${imageURL + item.backdrop_path}`
+                    : item.poster_path
+                    ? `${imageURL + item.poster_path}`
+                    : defaultAvatar
+                }
+                alt={item.title}
+              />
+            </div>
+          </li>
+        ))}
       </ul>
     </div>
   );
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,7 +1,27 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { fetchMovie } from "../redux/slices/movieSlice";
+
 export default function MovieDetails() {
+  const movie = useSelector((state) => state.movie.movie);
+  const loading = useSelector((state) => state.movie.loading);
+  const hasErrors = useSelector((state) => state.movie.hasErrors);
+  const dispatch = useDispatch();
+  const params = useParams();
+
+  useEffect(() => {
+    if (!Object.keys(movie).length) {
+      // Make API call only if no data in store
+      dispatch(fetchMovie(params.id));
+    }
+  }, []);
+
   return (
     <div>
-      <div>Movie details component.</div>
+      {loading && <div>Loading...</div>}
+      {hasErrors && <div>Errors...</div>}
+      <div>This is movie title: {movie.original_title}</div>
     </div>
   );
 }
