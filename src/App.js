457debc4ac620370745a64e60e30f3e2495b0309
FrozenHearth <vishudbz@gmail.com>
2022-02-23 02:09:52 +0530

    Refactored footer & additional cache case handled

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.scss";
 import "font-awesome/css/font-awesome.min.css";
 import { Navbar } from "./pages/Navbar";
 import { MainContent } from "./pages/MainContent";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Navbar />
       <MainContent />
+      <Footer />
     </>
   );
 }
